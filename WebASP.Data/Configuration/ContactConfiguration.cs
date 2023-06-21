a8859b733c55d206b495ed707db1bcf394db99e6
2k1QuangLinh <118100679+2k1QuangLinh@users.noreply.github.com>
2023-06-21 11:22:31 +0700

    Update Migration

---
+++ WebASP.Data/Configuration/ContactConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebASP.Data.Entities;
+
+namespace WebASP.Data.Configuration
+{
+    public class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Contacts");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.PhoneNumber).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Message).IsRequired();
+      
+        }
+    }
+}
---
+++ WebASP.Data/Configuration/OrderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebASP.Data.Entities;
+
+namespace WebASP.Data.Configuration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("OrderConfiguration");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+
+        }
+    }
+}
